a2ea63470284f871927633f2b4fbfa2c67b82f66
rocca <robertoflores2925@gmail.com>
2026-09-12 11:45:19 -0600

    Mi corazon es verde y mi sangre dorada

--- analizadorlexico/Estados.go
+++ analizadorlexico/Estados.go
@@ -1,6 +1,5 @@
 package analizadorlexico
 
-// TODO: funciones aux para los estados sin tranciciones
 type Estado struct {
 	Transiciones map[rune]*Estado
 	IsF          bool
--- analizadorlexico/Lexemas.go
+++ analizadorlexico/Lexemas.go
@@ -1,37 +1,34 @@
 package analizadorlexico
 
 type Lexema struct {
-	Q1      Estado
-	Estados []Estado
-	Token   string
+	QInicial Estado
+	Estados  []Estado
+	Token    string
 }
 
 func CrearLexema(estadoInicial Estado, listaEstados []Estado, nombreToken string) *Lexema {
 	var l Lexema
-	l.Q1 = estadoInicial
+	l.QInicial = estadoInicial
 	l.Estados = listaEstados
 	l.Token = nombreToken
 	return &l
 }
 
-// TODO: funciones aux para los estados sin tranciciones
 func (l *Lexema) D(r []rune) *Lexema {
-	qActual := &l.Q1
+	q := &l.QInicial
 	for _, v := range r {
-		siguienteEstado := qActual.d(v)
+		siguienteEstado := q.d(v)
 
 		//recibio un caracter no aceptado en el adf
 		if siguienteEstado == nil {
 			return nil
 		}
 
-		qActual = siguienteEstado
+		q = siguienteEstado
 	}
 
-	//termino en un estado F aceptado
-	if qActual.IsF {
+	if q.IsF {
 		return l
 	}
-	//no termino en un Estado Aceptado
 	return nil
 }
